b2c1ac8d8c08df3d77af9f728ea9cc0faf12309f
emmonbea <moskaleviluak@icloud.com>
2024-09-17 21:54:48 +0500

    test: add tests for Point3D structure

---
+++ src/tests/include/main.test.h
@@ -0,0 +1,19 @@
+/**
+ * @file main.test.h
+ * @author emmonbea (moskaleviluak@icloud.com)
+ * @brief
+ * @version 1.0
+ * @date 2024-09-17
+ *
+ * @copyright Copyright (c) 2024
+ *
+ */
+
+#ifndef SRC_TESTS_INCLUDE_MAIN_TEST_H_
+#define SRC_TESTS_INCLUDE_MAIN_TEST_H_
+
+#include <gtest/gtest.h>
+
+#include "../../include/model/3d_point.h"
+
+#endif  // SRC_TESTS_INCLUDE_MAIN_TEST_H_
---
+++ src/tests/main_test.cc
@@ -0,0 +1,21 @@
+/**
+ * @file main_test.cc
+ * @author emmonbea (moskaleviluak@icloud.com)
+ * @brief
+ * @version 1.0
+ * @date 2024-09-17
+ *
+ * @copyright Copyright (c) 2024
+ *
+ */
+
+#include "include/main.test.h"
+
+int main(int argc, char** argv) {
+  ::testing::InitGoogleTest(&argc, argv);
+  if (argc > 1) {
+    ::testing::GTEST_FLAG(filter) = argv[1];
+  }
+
+  return RUN_ALL_TESTS();
+}
