f4c732ac917d8e3ab61d0e49bc6996a4018f06e1
emmonbea <moskaleviluak@icloud.com>
2024-09-17 21:55:35 +0500

    feat: add main function to the project

--- src/3DViewer.cc
+++ src/3DViewer.cc
@@ -1,10 +1,12 @@
 /**
- * @file main.cc
+ * @file 3DViewer.cc
  * @author emmonbea (moskaleviluak@icloud.com)
- * @brief 
+ * @brief
  * @version 1.0
- * @date 2024-09-16
- * 
+ * @date 2024-09-17
+ *
  * @copyright Copyright (c) 2024
- * 
+ *
  */
+
+int main() { return 0; }
