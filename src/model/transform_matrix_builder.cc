63e19ec21c600de4017ad44fd1c34d4594150c4f
emmonbea <moskaleviluak@icloud.com>
2024-09-20 21:29:36 +0500

    feat: add implementation for creating transition matrices

--- src/model/transform_matrix_builder.cc
+++ src/model/transform_matrix_builder.cc
@@ -11,21 +11,49 @@
 
 #include "include/model/transform_matrix_builder.h"
 
+#include <cmath>
+
 namespace s21 {
 TransformMatrix TransformMatrixBuilder::CreateRotationMatrix(float x, float y,
                                                              float z) noexcept {
+  TransformMatrix rot_x, rot_y, rot_z;
+
+  rot_x.set_element(1, 1, cos(x));
+  rot_x.set_element(1, 2, -sin(x));
+  rot_x.set_element(2, 1, sin(x));
+  rot_x.set_element(2, 2, cos(x));
+
+  rot_y.set_element(0, 0, cos(y));
+  rot_y.set_element(0, 2, sin(y));
+  rot_y.set_element(2, 0, -sin(y));
+  rot_y.set_element(2, 2, cos(y));
+
+  rot_z.set_element(0, 0, cos(z));
+  rot_z.set_element(0, 1, -sin(z));
+  rot_z.set_element(1, 0, sin(z));
+  rot_z.set_element(1, 1, cos(z));
+
+  return rot_x * rot_y * rot_z;
 }
 
 TransformMatrix TransformMatrixBuilder::CreateMoveMatrix(float x, float y,
-                                                         float z) noexcept {}
+                                                         float z) noexcept {
+  TransformMatrix res;
+
+  res.set_element(0, 3, x);
+  res.set_element(1, 3, y);
+  res.set_element(2, 3, z);
+
+  return res;
+}
 
 TransformMatrix TransformMatrixBuilder::CreateScaleMatrix(float x, float y,
                                                           float z) noexcept {
   TransformMatrix matrix;
 
-  matrix.m_[0][0] = x;
-  matrix.m_[1][1] = y;
-  matrix.m_[2][2] = z;
+  matrix.set_element(0, 0, x);
+  matrix.set_element(1, 1, y);
+  matrix.set_element(2, 2, z);
 
   return matrix;
 }
