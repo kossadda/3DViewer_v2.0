1fbb7b882c8f0cadcf09d331137b9a9814555c1c
emmonbea <moskaleviluak@icloud.com>
2024-09-23 20:49:14 +0500

    feat: add controller class

---
+++ src/controller/facade.cc
@@ -0,0 +1,14 @@
+/**
+ * @file facade.cc
+ * @author emmonbea (moskaleviluak@icloud.com)
+ * @brief
+ * @version 1.0
+ * @date 2024-09-23
+ *
+ * @copyright Copyright (c) 2024
+ *
+ */
+
+#include "include/controller/facade.h"
+
+namespace s21 {}
---
+++ src/include/controller/facade.h
@@ -0,0 +1,25 @@
+/**
+ * @file facade.h
+ * @author emmonbea (moskaleviluak@icloud.com)
+ * @brief
+ * @version 1.0
+ * @date 2024-09-23
+ *
+ * @copyright Copyright (c) 2024
+ *
+ */
+
+#ifndef SRC_INCLUDE_CONTROLLER_FACADE_H_
+#define SRC_INCLUDE_CONTROLLER_FACADE_H_
+
+#include "include/model/scene.h"
+
+namespace s21 {
+class Facade {
+ public:
+ private:
+  Scene scene_;
+};
+}  // namespace s21
+
+#endif  // SRC_INCLUDE_CONTROLLER_FACADE_H_
