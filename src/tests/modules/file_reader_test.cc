490311212c609edd9396c899e32a355bcf6e70f0
emmonbea <moskaleviluak@icloud.com>
2024-09-25 22:24:44 +0500

    feat

---
+++ src/tests/modules/file_reader_test.cc
@@ -0,0 +1,14 @@
+/**
+ * @file file_reader_test.cc
+ * @author emmonbea (moskaleviluak@icloud.com)
+ * @brief 
+ * @version 1.0
+ * @date 2024-09-25
+ * 
+ * @copyright Copyright (c) 2024
+ * 
+ */
+
+#include "../include/main.test.h"
+#include "include/model/file_reader.h"
+
