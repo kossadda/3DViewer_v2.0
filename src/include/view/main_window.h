780f4bdc26ca26d2fbcddf54c55e564cb89fbc14
kossadda <gabilov1997@gmail.com>
2024-10-04 18:33:49 +0500

    feat: add MainWindow class feat: add CMakeLists.txt feat: add resource.qrc feat: add to Makefile targets all, install, uninstall

---
+++ src/include/view/main_window.h
@@ -0,0 +1,26 @@
+/**
+ * @file main_window.h
+ * @author kossadda (https://github.com/kossadda)
+ * @brief 
+ * @version 1.0
+ * @date 2024-10-04
+ * 
+ * @copyright Copyright (c) 2024
+ * 
+ */
+
+#ifndef SRC_INCLUDE_VIEW_MAIN_WINDOW_H
+#define SRC_INCLUDE_VIEW_MAIN_WINDOW_H
+
+#include <QWidget>
+
+class MainWindow : public QWidget {
+ public:
+  MainWindow();
+  ~MainWindow();
+
+ private:
+
+};
+
+#endif  // SRC_INCLUDE_VIEW_MAIN_WINDOW_H
---
+++ src/view/main_window.cc
@@ -0,0 +1,16 @@
+/**
+ * @file main_window.cc
+ * @author kossadda (https://github.com/kossadda)
+ * @brief 
+ * @version 1.0
+ * @date 2024-10-04
+ * 
+ * @copyright Copyright (c) 2024
+ * 
+ */
+
+#include "include/view/main_window.h"
+
+MainWindow::MainWindow() : QWidget{} {}
+
+MainWindow::~MainWindow() {}
